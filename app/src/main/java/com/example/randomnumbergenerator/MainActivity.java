23cc8f204c1fcf7a99a7c042aabe0fd94113e5d8
itwasntzak_window <zakary100@gmail.com>
2020-04-01 17:08:08 -0600

    mostly happy with how it currently works

--- app/src/main/java/com/example/randomnumbergenerator/MainActivity.java
+++ app/src/main/java/com/example/randomnumbergenerator/MainActivity.java
@@ -7,9 +7,11 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.InterruptedIOException;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+//  todo: add buttons below and above random range EditText, incrament value +/- 1 each press
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,36 +19,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-//    public void rangeOfRandom() {
-//        EditText rangeInput = findViewById(R.id.rollRangeInput);
-//        int randomRange = 2;
-//        rollTotal.setText(randomRange);
-//        rollTotal.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//                ;
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//                String text = rangeInput.getText().toString();
-//                randomRange = Integer.parseInt(text);
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable editable) {
-//
-//            }
-//        });
-//         rangeNumber;
-//    }
-
     public void rollRandom(View view) {
-//      todo: figure out how to get an integer from a number EditText, then use a parameter for random range
-        EditText rangeInput = findViewById(R.id.rollRangeInput);
-        Integer rollRange =  Integer.parseInt(rangeInput.getText().toString());
-        Integer randomNumb = new Random().nextInt(rollRange) + 1;
-        TextView textView = findViewById(R.id.textView);
+        Integer randomNumb = new Random().nextInt(randomRange()) + 1;
+        TextView textView = (TextView) findViewById(R.id.randomResults);
         textView.setText(randomNumb.toString());
+    }
+
+    public int randomRange() {
+        EditText rangeInput = (EditText) findViewById(R.id.rollRangeInput);
+        Integer rollRange =  Integer.parseInt(rangeInput.getText().toString());
+        return rollRange;
     }
 }
